c0c6a088407f2f2af98495a6fe327ac90a4514d7
dennis-developer-php <marteldennis05@gmail.com>
2020-07-30 12:43:28 -0600

    mern stack

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react'
+//llamando a axios
 import axios from 'axios'
+//llamando a datapicker
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default class CreateNote extends Component {
     state = {
         users: [],
-        //userSelected
+        userSelected: '',
+        title: '',
+        content: '',
+        date: new Date(),
+        editing: false,
+        _id: ''
     }
 
     async componentDidMount() {
         const res = await axios.get('http://localhost:4000/api/users');
-        this.setState({users: res.data.map(user => user.username)});
-        console.log(this.state.users);
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        });
+        //console.log(this.state.users);
+        if(this.props.match.params.id) {
+            const res = await axios.get('http://localhost:4000/api/notes/'+ this.props.match.params.id)
+            this.setState({
+                title: res.data.title,
+                content: res.data.content,
+                date: new Date(res.data.date),
+                author: res.data.author,
+                editing: true,
+                _id: this.props.match.params.id
+            })
+        }
+        
     }
 
-    _onSubmit = e => {
+    _onSubmit = async e => {
         e.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        }
+        if(this.state.editing) {
+            await axios.put('http://localhost:4000/api/notes/' + this.state._id, newNote)
+        } else {
+            await axios.post('http://localhost:4000/api/notes/', newNote);
+        }
+        //console.log(res);
+        window.location.href = "/"
     }
 
     _onChangeUsername = e => {
         this.setState({
-            userSelected: e.target.value
+            [e.target.name]: e.target.value
         })
         //console.log(e.target.value)
+    }
+
+    _onChangeDate = date => {
+        this.setState({date})
     }
 
     render() {
@@ -38,6 +80,7 @@ export default class CreateNote extends Component {
                                 className="form-control"
                                 name="userSelected"
                                 onChange={this._onChangeUsername}
+                                value={this.state.author}
                             >
                                 {
                                     this.state.users.map(user =>
@@ -55,6 +98,8 @@ export default class CreateNote extends Component {
                                 placeholder="title"
                                 className="form-control"
                                 required
+                                onChange={this._onChangeUsername}
+                                value={this.state.title}
                             />
                         </div>
                         <div className="form-group">
@@ -63,8 +108,18 @@ export default class CreateNote extends Component {
                                 className="form-control"
                                 placeholder="content"
                                 required
+                                onChange={this._onChangeUsername}
+                                value={this.state.content}
                                 >
                             </textarea>
+                        </div>
+                        <div className="form-group">
+                            <DatePicker 
+                                className="form-control"
+                                selected={this.state.date}
+                                onChange={this._onChangeDate}
+                                
+                            />
                         </div>
                         <form onSubmit={this._onSubmit}>
                             <div className="form-group">
--- frontend/src/components/NotesList.js
+++ frontend/src/components/NotesList.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import {format} from 'timeago.js'
+import {Link} from 'react-router-dom'
 
 export default class NotesList extends Component {
+    state = {
+        notes: []
+    }
+
+    componentDidMount() {
+        this.getNotes()
+    }
+
+    _deleteNote = async id => {
+        await axios.delete(`http://localhost:4000/api/notes/${id}`)
+        this.getNotes();
+    }
+
+    async getNotes() {
+        const res = await axios.get('http://localhost:4000/api/notes')
+        this.setState({notes: res.data})
+    }
+
     render() {
         return (
-            <div>
-                NotesList
+            <div className="row">
+                {
+                    this.state.notes.map(note => (
+                        <div className="col-md-3 p-2" key={this._id}>
+                            <div className="card">
+                                <div className="card-header d-flex justify-content-between">
+                                    <h6>
+                                        {note.title}
+                                    </h6>
+                                    <Link className="btn btn-warning" to={"/edit/" + note._id}>
+                                        edit
+                                    </Link>
+                                </div>
+                                <div className="card-body">
+                                    <p>{note.content}</p>
+                                    <p>{note.author}</p>
+                                    <p>{format(note.date)}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <button className="btn btn-danger" onClick={() => this._deleteNote(note.id)}>
+                                        delete
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
